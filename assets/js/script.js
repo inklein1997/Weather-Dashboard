f94394f329151029b6a3e1cf44588e112680961a
Michael Klein <Michaelklein1997@gmail.com>
2022-01-06 23:04:45 -0600

    current day weather data is now stored as an object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,49 @@
-// var latitude
-// var longitude
+var userInputCity
+var weatherData
+var currentWeather
+
+
+$('#submitCityInput').on('click', function() {
+    console.log("click!")
+    userInputCity = $('#cityInput').val();
+    console.log(userInputCity)
+    pullCoordinates()
+})
+
+function pullCoordinates() {
+    var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q='+userInputCity+'&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+    fetch(requestUrlLocation)
+    .then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        console.log(data);
+        var longitude = data.coord.lon
+        var latitude = data.coord.lat
+        console.log("longitude: " + longitude)
+        console.log("latitude: " + latitude)
+        function pullWeather() {
+            console.log(latitude)
+            var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+            fetch(requestUrl)
+            .then(function(response1) {
+                return response1.json()
+            }).then(function(data1) {
+                console.log(data1)
+                currentWeather = {
+                    temp: data1.current.temp,
+                    wind: data1.current.wind_speed,
+                    humidity: data1.current.humidity,
+                    uvindex: data1.current.uvi,
+                }
+                console.log(currentWeather)
+            }) 
+        }
+        pullWeather();
+    })  
+}
+
+
+
 
 var pullCoordinates = function() {
     var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
@@ -12,54 +56,19 @@ var pullCoordinates = function() {
         var latitude = data.coord.lat
         console.log("longitude: " + longitude)
         console.log("latitude: " + latitude)
-        function pullWeather() {
-            console.log(latitude)
-            var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-            fetch(requestUrl)
-            .then(function(response1) {
-                return response1.json()
-            }).then(function(data1) {
-                console.log(data1)
-            })
-        }
-        pullWeather();
+        return [latitude,longitude]
     })
     
 }
 
-
+function pullWeather() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + pullCoordinates[1] + '&lon=' + pullCoordinates[0] + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+    fetch(requestUrl)
+    .then(function(response1) {
+        return response1.json()
+    }).then(function(data1) {
+        console.log(data1)
+    })
+}
 
 console.log(pullCoordinates());
-// pullCoordinates()
-// pullWeather(pullCoordinates());
-
-
-
-// var pullCoordinates = function() {
-//     var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-//     fetch(requestUrlLocation)
-//     .then(function(response) {
-//         return response.json()
-//     }).then(function(data) {
-//         console.log(data);
-//         var longitude = data.coord.lon
-//         var latitude = data.coord.lat
-//         console.log("longitude: " + longitude)
-//         console.log("latitude: " + latitude)
-//         return [longitude, latitude]
-//     })
-    
-// }
-
-// function pullWeather() {
-//     var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + pullCoordinates[1] + '&lon=' + pullCoordinates[0] + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-//     fetch(requestUrl)
-//     .then(function(response1) {
-//         return response1.json()
-//     }).then(function(data1) {
-//         console.log(data1)
-//     })
-// }
-
-// console.log(pullCoordinates());
-// ;
