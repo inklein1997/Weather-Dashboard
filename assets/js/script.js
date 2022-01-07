b818443f7f9ff74dabc298c0730c75dd12b85d4b
Michael Klein <Michaelklein1997@gmail.com>
2022-01-07 09:46:27 -0600

    cleaned code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var userInputCity
 var weatherData
-var currentWeather
 
 
 $('#submitCityInput').on('click', function() {
@@ -21,54 +20,27 @@ function pullCoordinates() {
         var latitude = data.coord.lat
         console.log("longitude: " + longitude)
         console.log("latitude: " + latitude)
-        function pullWeather() {
-            console.log(latitude)
-            var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-            fetch(requestUrl)
-            .then(function(response1) {
-                return response1.json()
-            }).then(function(data1) {
-                console.log(data1)
-                currentWeather = {
-                    temp: data1.current.temp,
-                    wind: data1.current.wind_speed,
-                    humidity: data1.current.humidity,
-                    uvindex: data1.current.uvi,
-                }
-                console.log(currentWeather)
-            }) 
-        }
-        pullWeather();
+        pullWeather(longitude, latitude);
     })  
 }
 
-
-
-
-var pullCoordinates = function() {
-    var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-    fetch(requestUrlLocation)
-    .then(function(response) {
-        return response.json()
-    }).then(function(data) {
-        console.log(data);
-        var longitude = data.coord.lon
-        var latitude = data.coord.lat
-        console.log("longitude: " + longitude)
-        console.log("latitude: " + latitude)
-        return [latitude,longitude]
-    })
-    
-}
-
-function pullWeather() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + pullCoordinates[1] + '&lon=' + pullCoordinates[0] + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+function pullWeather(longitude, latitude) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
     fetch(requestUrl)
     .then(function(response1) {
         return response1.json()
     }).then(function(data1) {
         console.log(data1)
-    })
+        weatherData = {
+            temp: data1.current.temp,
+            wind: data1.current.wind_speed,
+            humidity: data1.current.humidity,
+            uvindex: data1.current.uvi,
+        }
+        console.log(currentWeather)
+    }) 
 }
 
-console.log(pullCoordinates());
+function displayData() {
+
+}
