8cc6499f8146dc7fd58975b86d2cb272e319b0a4
Michael Klein <Michaelklein1997@gmail.com>
2022-01-06 22:27:20 -0600

    fetches are now functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var latitude
-var longitude
+// var latitude
+// var longitude
 
 var pullCoordinates = function() {
     var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
@@ -8,23 +8,58 @@ var pullCoordinates = function() {
         return response.json()
     }).then(function(data) {
         console.log(data);
-        longitude = data.coord.lon
-        latitude = data.coord.lat
+        var longitude = data.coord.lon
+        var latitude = data.coord.lat
         console.log("longitude: " + longitude)
         console.log("latitude: " + latitude)
-        return [longitude, latitude]
+        function pullWeather() {
+            console.log(latitude)
+            var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+            fetch(requestUrl)
+            .then(function(response1) {
+                return response1.json()
+            }).then(function(data1) {
+                console.log(data1)
+            })
+        }
+        pullWeather();
     })
+    
 }
 
-function pullWeather(pullCoordinates) {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + pullCoordinates[1] + '&lon=' + pullCoordinates[0] + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-    fetch(requestUrl)
-    .then(function(response1) {
-        return response1.json()
-    }).then(function(data1) {
-        console.log(data1)
-    })
-}
 
-pullCoordinates()
-pullWeather()
+
+console.log(pullCoordinates());
+// pullCoordinates()
+// pullWeather(pullCoordinates());
+
+
+
+// var pullCoordinates = function() {
+//     var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+//     fetch(requestUrlLocation)
+//     .then(function(response) {
+//         return response.json()
+//     }).then(function(data) {
+//         console.log(data);
+//         var longitude = data.coord.lon
+//         var latitude = data.coord.lat
+//         console.log("longitude: " + longitude)
+//         console.log("latitude: " + latitude)
+//         return [longitude, latitude]
+//     })
+    
+// }
+
+// function pullWeather() {
+//     var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + pullCoordinates[1] + '&lon=' + pullCoordinates[0] + '&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+//     fetch(requestUrl)
+//     .then(function(response1) {
+//         return response1.json()
+//     }).then(function(data1) {
+//         console.log(data1)
+//     })
+// }
+
+// console.log(pullCoordinates());
+// ;
