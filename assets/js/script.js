a9485bf654cc7632a157ef09c644aa47f24bfb0b
Michael Klein <Michaelklein1997@gmail.com>
2022-01-07 16:10:26 -0600

    current weather data now appends to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var userInputCity
-var weatherData
+var currentWeatherData
 
-
+var weatherData = []
 $('#submitCityInput').on('click', function() {
     console.log("click!")
     userInputCity = $('#cityInput').val();
@@ -25,22 +25,43 @@ function pullCoordinates() {
 }
 
 function pullWeather(longitude, latitude) {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
-    fetch(requestUrl)
-    .then(function(response1) {
-        return response1.json()
-    }).then(function(data1) {
-        console.log(data1)
-        weatherData = {
-            temp: data1.current.temp,
-            wind: data1.current.wind_speed,
-            humidity: data1.current.humidity,
-            uvindex: data1.current.uvi,
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
+    fetch(weatherUrl)
+    .then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        console.log(data)
+        var currentWeatherData = {
+            weather: data.current.weather[0].main,
+            temp: data.current.temp,
+            wind: data.current.wind_speed,
+            humidity: data.current.humidity,
+            uvindex: data.current.uvi,
         }
-        console.log(currentWeather)
+        weatherData.push(currentWeatherData)
+        console.log(currentWeatherData)
+        for(var i = 0; i < 5; i++) {
+            var futureWeatherData = {
+                weather: data.daily[i].weather[0].main,
+                temp: data.daily[i].temp,
+                wind: data.daily[i].wind_speed,
+                humidity: data.daily[i].humidity,
+            }
+            weatherData.push(futureWeatherData)
+        }
+        console.log(weatherData)
+        displayData()
     }) 
 }
-
+// var sectionEl = ('section')
 function displayData() {
-
+var divEl = $('<div>')
+.addClass("col border mb-3")
+.append($('<h2>').text(userInputCity),
+$('<h2>').text(weatherData[0].temp),
+$('<h2>').text(weatherData[0].wind_speed),
+$('<h2>').text(weatherData[0].humidity),
+$('<h2>').text(weatherData[0].uvindex),
+)
+$('section').append(divEl)
 }
