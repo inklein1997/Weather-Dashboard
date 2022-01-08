5a607fcf139eda2229b95209debb0bbf873486b8
Michael Klein <Michaelklein1997@gmail.com>
2022-01-07 19:03:29 -0600

    fixed bug that prevented user from deleting multiple searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,10 @@ $('#submitCityInput').on('click', function(event) {
 })
 
 function addPreviousSearchButton() {
-    $('#previousSearchList').append($('<div>').addClass("hstack gap-3").attr('id','previous-search')
+    var divEl = $('<div>').addClass("hstack gap-3").attr('id','previous-search').on('click','#delete-button',function(event) {
+        $(event.target).parent().remove()
+    })
+    $('#previousSearchList').append(divEl
         .append(
             $('<button>').addClass("btn btn-primary container-fluid").text(userInputCity),
             $('<div>').addClass("vr"),
@@ -27,10 +30,6 @@ function resetData() {
     weatherData= []
     currentWeatherData=""
 }
-
-$('#previous-search').on('click', '#delete-button' ,function(event) {
-    $(event.target).parent().remove()
-})
 
 function pullCoordinates() {
     var requestUrlLocation = 'https://api.openweathermap.org/data/2.5/weather?q='+userInputCity+'&appid=9d7ebf8b022f99c1559d4339ab5c60ee'
